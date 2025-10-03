ecb417a3cf30ab58ce19b43029f8e793e81402a5
hadarge <hadar111@gmail.com>
2025-10-03 00:36:04 -0400

    Sonda

--- packages/reactApp/vite.config.ts
+++ packages/reactApp/vite.config.ts
@@ -2,17 +2,20 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+//import Sonda from 'sonda/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        //  Sonda(),
     ],
     server: {
         port: 2222,
     },
     build: {
+        // sourcemap: true,
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, 'index.html'),
